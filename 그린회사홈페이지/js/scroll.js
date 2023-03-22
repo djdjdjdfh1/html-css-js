96603229c3ede0e39a638a65d0316db34f06d355
Park Chan <qkrcks8028@gmail.com>
2023-03-22 13:07:11 +0900

    0322 연습

---
+++ 그린회사홈페이지/js/scroll.js
@@ -0,0 +1,10 @@
+/* 스크롤 이벤트 작성 */
+// 스크롤 할때마다 실행되는 함수
+
+// 네비게이션 바 
+const nav = document.querySelector("#nav")
+
+window.addEventListener("scroll", function() {
+    // 스크롤 되면 scrollon 클래스 추가
+    nav.classList.add("scrollon");
+    })
